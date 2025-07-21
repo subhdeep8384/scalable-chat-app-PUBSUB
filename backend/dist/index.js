fed926c497694cc8167664036ae8ea36f34ecf74
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-07-21 13:25:55 +0530

    pubsub added

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,8 +1,41 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
+const redis_1 = require("redis");
 // creating a websocket server
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const port = 8081;
+const wss = new ws_1.WebSocketServer({ port });
+// created two seperate client for redis 
+const clients = {};
+function connectToRedis() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const publishClient = (0, redis_1.createClient)();
+        const subscribeClient = (0, redis_1.createClient)();
+        yield publishClient.connect();
+        yield subscribeClient.connect();
+        return { publishClient, subscribeClient };
+    });
+}
+;
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { publishClient, subscribeClient } = yield connectToRedis();
+        clients.publishClient = publishClient;
+        clients.subscribeClient = subscribeClient;
+        console.log("redis started", clients);
+    }
+    catch (e) { }
+}))();
+console.log("server started at :", port);
 const subscription = {};
 wss.on("connection", (ws) => {
     console.log("ws have connected");
@@ -12,21 +45,38 @@ wss.on("connection", (ws) => {
         rooms: [],
     };
     ws.on("message", (message) => {
-        var _a, _b;
-        const userMessage = JSON.parse(message.toString());
-        if (((_a = userMessage.type) === null || _a === void 0 ? void 0 : _a.toLowerCase()) === "SUBSCRIBE".toLowerCase()) {
-            subscription[id].rooms.push(userMessage.roomId);
-            console.log("Subscribe to :", userMessage.roomId);
-        }
-        if (((_b = userMessage.type) === null || _b === void 0 ? void 0 : _b.toLowerCase()) === "sendMessage".toLowerCase()) {
-            const message = userMessage.message;
-            const roomId = userMessage.roomId;
-            Object.keys(subscription).forEach((userId) => {
-                const { ws, rooms } = subscription[userId];
-                if (rooms.includes(roomId)) {
+        var _a, _b, _c, _d, _e;
+        try {
+            const userMessage = JSON.parse(message.toString());
+            if (((_a = userMessage.type) === null || _a === void 0 ? void 0 : _a.toLowerCase()) === "SUBSCRIBE".toLowerCase()) {
+                const { type, roomId } = userMessage;
+                // subscription[id].rooms.push(userMessage.roomId) ;
+                // console.log("Subscribe to :", userMessage.roomId);
+                (_b = clients.subscribeClient) === null || _b === void 0 ? void 0 : _b.subscribe(roomId, (message) => {
                     ws.send(message);
-                }
-            });
+                });
+            }
+            if (((_c = userMessage.type) === null || _c === void 0 ? void 0 : _c.toLowerCase()) === "INSUBSCRIBE".toLowerCase()) {
+                subscription[id].rooms = subscription[id].rooms.filter(x => x !== userMessage.roomId);
+            }
+            if (((_d = userMessage.type) === null || _d === void 0 ? void 0 : _d.toLowerCase()) === "sendMessage".toLowerCase()) {
+                const message = userMessage.message;
+                const roomId = userMessage.roomId;
+                // Object.keys(subscription).forEach((userId) => {
+                //     const { ws, rooms } = subscription[userId];
+                //     if (rooms.includes(roomId)) {
+                //         ws.send(message);
+                //     }
+                // })
+                (_e = clients.publishClient) === null || _e === void 0 ? void 0 : _e.publish(roomId, JSON.stringify({
+                    type: "sendMessage",
+                    roomId,
+                    message
+                }));
+            }
+        }
+        catch (e) {
+            console.log(e);
         }
     });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,46 +1,95 @@
-import {WebSocketServer , WebSocket , RawData } from "ws" ;
-import {createClient } from "redis"
+import { WebSocketServer, WebSocket, RawData } from "ws";
+import { createClient, RedisClientType } from "redis"
 
 // creating a websocket server
-const wss = new WebSocketServer({port : 8080}) ;
- 
-const subscription : {[key : string ]: {
-    ws : WebSocket ,
-    rooms : string[] 
-}}= {}
+const port = 8081 ;
+const wss = new WebSocketServer({ port });
 
-wss.on("connection" , (ws : WebSocket )=> {
+// created two seperate client for redis 
+const clients : {
+    publishClient? : ReturnType<typeof createClient>
+    subscribeClient? : ReturnType<typeof createClient>
+} = {} ;
+async function connectToRedis(){
+    const publishClient = createClient() ;
+    const subscribeClient = createClient() ;  
+    
+    await  publishClient.connect() ;
+    await subscribeClient.connect()
+    return { publishClient , subscribeClient }
+};
+
+(async () =>{
+    try{
+        const { publishClient , subscribeClient } = await connectToRedis() ;
+        
+        clients.publishClient = publishClient ;
+        clients.subscribeClient = subscribeClient ;
+        console.log("redis started" , clients)
+
+    }catch(e){}
+})() ;
+
+console.log("server started at :",port)
+
+const subscription: {
+    [key: string]: {
+        ws: WebSocket,
+        rooms: string[]
+    }
+} = {}
+
+wss.on("connection", (ws: WebSocket) => {
     console.log("ws have connected")
-    const id = randomId() ;
+    const id = randomId();
 
     subscription[id] = {
-        ws ,
-        rooms : [] ,
-    } 
+        ws,
+        rooms: [],
+    }
 
-    ws.on("message" , (message : {message : RawData })=>{
-       const userMessage = JSON.parse(message.toString())
-       if(userMessage.type?.toLowerCase() ==="SUBSCRIBE".toLowerCase()){
-        subscription[id].rooms.push(userMessage.roomId) ;
-        console.log("Subscribe to :" , userMessage.roomId) ;
-       }
+    ws.on("message", (message: { message: RawData }) => {
+        try {
+            const userMessage = JSON.parse(message.toString())
+            if (userMessage.type?.toLowerCase() === "SUBSCRIBE".toLowerCase()) {
+                const {type , roomId} = userMessage ;
+                // subscription[id].rooms.push(userMessage.roomId) ;
+                // console.log("Subscribe to :", userMessage.roomId);
 
+                clients.subscribeClient?.subscribe(roomId ,(message : string)=>{
+                   ws.send(message)
+                })
 
-       if(userMessage.type?.toLowerCase() === "sendMessage".toLowerCase()){
-        const message = userMessage.message ;
-        const roomId = userMessage.roomId ;
-
-        Object.keys(subscription).forEach((userId)=>{
-            const {ws , rooms} = subscription[userId] ;
-            if(rooms.includes(roomId)){
-                ws.send(message) ;
             }
-        });
-    }  
+
+            if(userMessage.type?.toLowerCase() === "INSUBSCRIBE".toLowerCase()){
+                subscription[id].rooms = subscription[id].rooms.filter(x => x !== userMessage.roomId)
+            }
+
+
+            if (userMessage.type?.toLowerCase() === "sendMessage".toLowerCase()) {
+                const message = userMessage.message;
+                const roomId = userMessage.roomId;
+
+                // Object.keys(subscription).forEach((userId) => {
+                //     const { ws, rooms } = subscription[userId];
+                //     if (rooms.includes(roomId)) {
+                //         ws.send(message);
+                //     }
+                // })
+                clients.publishClient?.publish(roomId, JSON.stringify({
+                    type : "sendMessage" ,
+                    roomId ,
+                    message 
+                }))
+            }
+        }catch(e){
+            console.log(e)
+        }
     })
-}) ;
+});
 
 
-function randomId(){
-    return Math.random() ;
+function randomId() {
+    return Math.random();
 }
